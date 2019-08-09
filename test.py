57637ff70eb2399362fa6ef7913e06e9101ba6fa
Bo Sun <sunbo804400200@gmail.com>
2019-08-09 17:31:14 -0400

    suceeded to detect the mini map

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ def CannyEdge(image):
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     cannyImage = cv2.Canny(blur, 50, 150)
     return cannyImage
+
 def region_of_interest(image): 
     #get the resolution of the image
     height, width = image.shape
     #set up the map extracting area
-    map_height_limit = int(0.65*height)
-    map_width_limit = int(0.8*width)
+    map_height_limit = int(0.6*height)
+    map_width_limit = int(0.82*width)
     #set the cropping polygons
     crop_area = np.array([[(map_width_limit, height),(map_width_limit, map_height_limit),(width, map_height_limit),(width, height),]], np.int32)
     #set the background of the mask to 0
@@ -34,12 +35,12 @@ def finding_minimap(image, lines):
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             #get verdical/horizontal lines in the mini map
-            if (abs(x1-x2)<3):#verdical boudary
+            if (abs(x1-x2)<2):#verdical boudary
                 xmin=min(x1,x2)
                 if (xmin<ver_boudary):
                     ver_boudary=xmin
                     mapcentre_x = int((xmin+x)/2)
-            if (abs(y1-y2)<3):#horizontal boudary
+            if (abs(y1-y2)<2):#horizontal boudary
                 ymin=min(y1,y2)
                 if (ymin<hor_boudary):
                     hor_boudary=ymin
@@ -56,7 +57,17 @@ def finding_minimap(image, lines):
     cv2.circle(map, centre, 5, (0,255,255), -2)
     return map
 
-image = cv2.imread('./testImage/replay-tool.jpg')
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 1)
+    return line_image
+
+#image = cv2.imread('./testImage/replay-tool.jpg')
+image = cv2.imread('./testImage/5.png')
+image = cv2.resize(image,(800,450))
 canny = CannyEdge(image)#edge detection
 cropped_Image = region_of_interest(canny)#get the mini map area
 
@@ -65,16 +76,33 @@ rho = 2
 theta = np.pi/180
 threshold = 80
 #Hough Transformation
-lines = cv2.HoughLinesP(cropped_Image,rho, theta, threshold, np.array ([]), minLineLength=10, maxLineGap=5)
+lines = cv2.HoughLinesP(cropped_Image,rho, theta, threshold, np.array ([]), minLineLength=20, maxLineGap=1)
 #Get the lines
 map_info = finding_minimap(image, lines)
+hough = display_lines(image, lines)
+#edge_hough = cv2.addWeighted(canny, 0.8, hough, 1, 1)
+#hough_mapinfo=cv2.addWeighted(map_info, 0.8, hough, 1, 1)
 #Display the minmap edges/boudaries in the original pic
 combo_image = cv2.addWeighted(map_info, 0.8, image, 1, 1)
 #showing the image
-
+canny_cvt = cv2.cvtColor(canny,cv2.COLOR_GRAY2RGB)
+ana = cv2.addWeighted(canny_cvt, 0.8, hough, 1, 1)
+'''
 cv2.imshow('result',combo_image)
 cv2.waitKey(0)
 '''
+plt.figure()
 plt.imshow(cropped_Image)
+
+plt.figure()
+plt.imshow(hough)
+
+plt.figure()
+plt.imshow(ana)
+
+plt.figure()
+plt.imshow(map_info)
+
+plt.figure()
+plt.imshow(combo_image)
 plt.show()
-'''
